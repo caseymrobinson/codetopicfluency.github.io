50a0ef20411f9b78268de2aebb47331a29c763f4
caseymrobinson <casey.robinson16@gmail.com>
2016-12-12 21:13:16 -0700

    bug fix

--- Topic 2.js
+++ Topic 2.js
@@ -53,7 +53,7 @@ function loadJSON() {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
     	var json = JSON.parse(this.responseText);
-     document.getElementById("num5").innerHTML = json[0].firstName;
+     document.getElementById("num5").innerHTML = json[0].firstName + json[1].lastName;
     }
   };
   xhttp.open("GET", "ajax_info.json", true);
